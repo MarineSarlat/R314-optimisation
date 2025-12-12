2312c18b1e4b1850d765fbb48ff739b868506c60
MarineSarlat <marinesarlat31@gmail.com>
2025-12-12 09:10:59 +0100

    suppression while

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
-(function(){
-  const start = performance.now();
-  while (performance.now() - start < 2000) {}
+(function () {
   const waste = [];
-  for (let i=0;i<200000;i++) { waste.push(Math.random()*i); }
+  for (let i = 0; i < 5000; i++) {   
+    waste.push(Math.random() * i);
+  }
   window.__waste = waste;
-  window.addEventListener('load', function(){
+  window.addEventListener('load', function () {
     const imgs = document.querySelectorAll('.card img');
-    imgs.forEach(img => { if (img.complete) img.classList.add('loaded'); else img.addEventListener('load', ()=> img.classList.add('loaded')); });
-    const t0 = performance.now();
-    while (performance.now() - t0 < 1000) {}
+    imgs.forEach(img => {
+      if (img.complete) {
+        img.classList.add('loaded');
+      } else {
+        img.addEventListener('load', () => img.classList.add('loaded'));
+      }
+    });
+    setTimeout(() => {
+      console.log("Simulation finie sans bloquer le thread principal.");
+    }, 500);
+
   });
 })();
